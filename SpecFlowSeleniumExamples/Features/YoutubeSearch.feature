63420515f557b2074abf53dcf250197a922b6b15
Anchita <anchita.dogra@gmail.com>
2023-07-18 10:50:57 +0100

    Add project files.

---
+++ SpecFlowSeleniumExamples/Features/YoutubeSearch.feature
@@ -0,0 +1,9 @@
+﻿Feature: YoutubeSearchFeature In order to test search functionality on youtube
+As a developer
+I want to ensure functionality is working end to end
+@mytag
+Scenario: Youtube should search for the given keyword and should navigate to search results page
+Given I have navigated to youtube website
+And I have entered England as search keyword
+When I press the search button
+Then I should be navigate to search results page
---
+++ SpecFlowSeleniumExamples/StepDefinitions/YoutubeSearchStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowSeleniumExamples.StepDefinitions
+{
+    [Binding]
+    public class YoutubeSearchStepDefinitions
+    {
+        [Given(@"I have navigated to youtube website")]
+        public void GivenIHaveNavigatedToYoutubeWebsite()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I have entered England as search keyword")]
+        public void GivenIHaveEnteredEnglandAsSearchKeyword()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I press the search button")]
+        public void WhenIPressTheSearchButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should be navigate to search results page")]
+        public void ThenIShouldBeNavigateToSearchResultsPage()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
