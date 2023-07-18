d54720a9a217023d1fb8cafb830e750814d1b652
Anchita <anchita.dogra@gmail.com>
2023-07-18 13:57:44 +0100

    Added steps for youtube search tests

--- SpecFlowSeleniumExamples/StepDefinitions/YoutubeSearchStepDefinitions.cs
+++ SpecFlowSeleniumExamples/StepDefinitions/YoutubeSearchStepDefinitions.cs
@@ -1,33 +1,67 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using TechTalk.SpecFlow;
 
 namespace SpecFlowSeleniumExamples.StepDefinitions
 {
     [Binding]
-    public class YoutubeSearchStepDefinitions
+
+    public class YoutubeSearchStepDefinitions : IDisposable
     {
+        private ChromeDriver chromeDriver;
+        private string searchKeyword;
+
+        public YoutubeSearchStepDefinitions()
+        {
+            chromeDriver = new ChromeDriver("driverpath");
+        }
+ 
+
         [Given(@"I have navigated to youtube website")]
         public void GivenIHaveNavigatedToYoutubeWebsite()
         {
-            throw new PendingStepException();
+            chromeDriver.Navigate().GoToUrl("https://www.youtube.com");
+            Assert.IsTrue(chromeDriver.Title.ToLower().Contains("youtube"));
         }
 
-        [Given(@"I have entered England as search keyword")]
-        public void GivenIHaveEnteredEnglandAsSearchKeyword()
+        [Given(@"I have entered (.*) as search keyword")]
+        public void GivenIHaveEnteredEnglandAsSearchKeyword(String searchString)
         {
-            throw new PendingStepException();
+            searchKeyword = searchString.ToLower();
+            var searchInputBox = chromeDriver.FindElement(By.Id("search"));
+            var wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("search")));
+            searchInputBox.SendKeys(searchKeyword);
         }
 
         [When(@"I press the search button")]
         public void WhenIPressTheSearchButton()
         {
-            throw new PendingStepException();
+            var searchButton = chromeDriver.FindElement(By.CssSelector("button#search-icon-legacy"));
+            searchButton.Click();
         }
 
         [Then(@"I should be navigate to search results page")]
         public void ThenIShouldBeNavigateToSearchResultsPage()
         {
-            throw new PendingStepException();
+            System.Threading.Thread.Sleep(2000);
+
+            Assert.IsTrue(chromeDriver.Url.ToLower().Contains(searchKeyword));
+            Assert.IsTrue(chromeDriver.Title.ToLower().Contains(searchKeyword));
+        }
+
+        public void Dispose()
+        {
+            if (chromeDriver != null)
+            {
+                chromeDriver.Dispose();
+                chromeDriver = null;
+            }
         }
     }
 }
